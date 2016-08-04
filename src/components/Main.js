f26315d7b22438801c91b81ec0cc022610405a8a
wangzicheng <wangzicheng@maimiaotech.com>
2016-08-04 15:13:29 +0800

    添加图片 搭建基本框架

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,58 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
-
+require('styles/App.scss');
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+//获取图片信息
+let imageData = require('json!../data/imageData.json');
+
+//图片添加地址
+imageData = (function(imageDataArr){
+    for (let i = 0, l = imageDataArr.length;i < l; i++) {
+        let singleImageData = imageDataArr[i]
+        singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+    }
+    return imageDataArr
+})(imageData)
+
+
+class ImgFigure extends React.Component {
+    render() {
+        return (
+            <figure>
+                <img src={this.props.data.imageURL} alt={this.props.data.title}/>
+                <figcaption>
+                    <h2>{this.props.data.title}</h2>
+                </figcaption>
+            </figure>
+        );
+    }
+}
 
 class AppComponent extends React.Component {
-  render() {
-    return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
-    );
-  }
+
+    componentDidMount() {
+
+    }
+    render() {
+        let controlerUnits = [],
+           imgFigures = [];
+        imageData.forEach(function(value){
+            imgFigures.push(<ImgFigure data={value}></ImgFigure>)
+        })
+
+        return (
+            <section className="stage">
+                <section className="img-sec">
+                    {imgFigures}
+                </section>
+                <nav className="controller-nav">
+                    {controlerUnits}
+                </nav>
+            </section>
+        );
+    }
 }
+
 
 AppComponent.defaultProps = {
 };
