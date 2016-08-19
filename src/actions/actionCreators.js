0f52386de2d237c2d6dba25debcfcf7708b7ddbe
wangzicheng <wangzicheng@maimiaotech.com>
2016-08-19 21:54:27 +0800

    修改component 由于通过bindActionCreators方法将dispatch和action绑定，在component中无需调用dispatch方法 可以直接使用props中的action方法触发

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -1,5 +1,12 @@
 // 获取首页余额
-import {GET_CAMPAIGNS, GET_BALANCE } from '../constants/index';
+import {
+    GET_CAMPAIGNS,
+    GET_BALANCE,
+    REQUEST_POSTS,
+    RECEIVE_POSTS,
+    SELECT_REDDIT,
+    INVALIDATE_REDDIT
+} from '../constants';
 
 export function getBalance(balance) {
     return {
@@ -15,3 +22,63 @@ export function getCampaigns(campaigns) {
 }
 
 
+// ajax
+import fetch from 'isomorphic-fetch'
+
+export function selectReddit(reddit) {
+  return {
+    type: SELECT_REDDIT,
+    reddit
+  }
+}
+
+export function invalidateReddit(reddit) {
+  return {
+    type: INVALIDATE_REDDIT,
+    reddit
+  }
+}
+
+function requestPosts(reddit) {
+  return {
+    type: REQUEST_POSTS,
+    reddit
+  }
+}
+
+function receivePosts(reddit, json) {
+  return {
+    type: RECEIVE_POSTS,
+    reddit,
+    posts: json.data.children.map(child => child.data),
+    receivedAt: Date.now()
+  }
+}
+
+function fetchPosts(reddit) {
+  return dispatch => {
+    dispatch(requestPosts(reddit))
+    return fetch(`https://www.reddit.com/r/${reddit}.json`)
+      .then(response => response.json())
+      .then(json => dispatch(receivePosts(reddit, json)))
+  }
+}
+
+function shouldFetchPosts(state, reddit) {
+  const posts = state.postsByReddit[reddit]
+  if (!posts) {
+    return true
+  }
+  if (posts.isFetching) {
+    return false
+  }
+  return posts.didInvalidate
+}
+
+export function fetchPostsIfNeeded(reddit) {
+  return (dispatch, getState) => {
+    if (shouldFetchPosts(getState(), reddit)) {
+      return dispatch(fetchPosts(reddit))
+    }
+  }
+}
--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -2,29 +2,68 @@
 
 import React from 'react';
 
-import { fetchPostsIfNeeded } from '../actions/syncActions'
-
 require('styles//Test.scss');
 
 class TestComponent extends React.Component {
-  componentDidMount() {
-    const { dispatch, selectedReddit } = this.props
-    dispatch(fetchPostsIfNeeded(selectedReddit))
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.selectedReddit !== this.props.selectedReddit) {
-      const { dispatch, selectedReddit } = nextProps
-      dispatch(fetchPostsIfNeeded(selectedReddit))
+    constructor(props) {
+        super(props)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleRefreshClick = this.handleRefreshClick.bind(this)
     }
-  }
-  render() {
-    return (
-      <div className="test-component">
-        Please edit src/components///TestComponent.js to update this component!
-      </div>
-    );
-  }
+    componentDidMount() {
+        const {  selectedReddit, fetchPostsIfNeeded } = this.props
+        fetchPostsIfNeeded(selectedReddit)
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.selectedReddit !== this.props.selectedReddit) {
+            const { fetchPostsIfNeeded, selectedReddit } = nextProps
+            fetchPostsIfNeeded(selectedReddit)
+        }
+    }
+    handleChange(nextReddit) {
+        this.props.selectReddit(nextReddit)
+    }
+    handleRefreshClick(e) {
+        e.preventDefault()
+
+        const {selectedReddit, invalidateReddit, fetchPostsIfNeeded } = this.props
+        invalidateReddit(selectedReddit)
+        fetchPostsIfNeeded(selectedReddit)
+    }
+    renderPosts(post, i) {
+        return (<li key={i}>{post.title}</li>)
+    }
+    render() {
+        const { posts, isFetching, lastUpdated } = this.props
+        const isEmpty = posts.length === 0
+        return (
+            <div>
+                <p>
+                    {lastUpdated &&
+                        <span>
+                          Last updated at {new Date(lastUpdated).toLocaleTimeString()}.
+                          {' '}
+                        </span>
+                    }
+                    {!isFetching &&
+                        <a href="#"
+                           onClick={this.handleRefreshClick}>
+                            Refresh
+                        </a>
+                    }
+                </p>
+                {isEmpty
+                    ? (isFetching ? <h2>Loading...</h2> : <h2>Empty.</h2>)
+                    : <div style={{ opacity: isFetching ? 0.5 : 1 }}>
+                        <ul>
+                            {posts.map(this.renderPosts)}
+                        </ul>
+                    </div>
+                }
+            </div>
+        )
+    }
 }
 
 TestComponent.displayName = 'TestComponent';
